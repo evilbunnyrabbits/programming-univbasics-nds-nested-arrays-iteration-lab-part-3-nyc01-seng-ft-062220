d51936d175ffff16139b5e55b5681a1d12360241
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-05-29 16:00:07 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,11 @@
 def join_nested_strings(src)
-  # src will be an Array of Arrays of Strings and Integers
-  # Combine all Strings present in the AoA into a single value and return it
+  strings = []
+  for item in array
+    for item in item
+      if item.class == String
+        strings.push(item)
+      end
+    end
+  end
+  return strings
 end
