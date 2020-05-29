5990ecce9289ef9286ea8b76011d1905cf34a2c6
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-05-29 16:00:45 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,9 +1,8 @@
 def join_nested_strings(src)
-  strings = []
   for item in array
     for item in item
       if item.class == String
-        strings.push(item)
+        puts item
       end
     end
   end
